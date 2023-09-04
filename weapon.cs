eeaa37029ebb1680221e4fc409047a5efe6f9806
NotATree1 <144020936+NotATree1@users.noreply.github.com>
2023-09-04 15:55:28 +0200

    Create weapon.cs

---
+++ weapon.cs
@@ -0,0 +1,12 @@
+class Weapon
+{
+    public int weapon_id;
+    public string weapon_name_shown;
+    public int power;
+    public Weapon(int weapon_id, string weapon_name_shown, int weapon_power)
+    {
+        this.weapon_id = weapon_id;
+        this.weapon_name_shown = weapon_name_shown;
+        this.weapon_power = weapon_power
+    }
+}
