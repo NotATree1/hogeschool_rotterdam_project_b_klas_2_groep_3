a900337af6310b52a28d26e60458179da667571a
NotaTree1 <rowankiksen@gmail.com>
2023-09-08 10:32:18 +0200

    started the move function
    Signed-off-by: NotaTree1 <rowankiksen@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,16 +1,25 @@
 ﻿// this is the code where we should start
 
-
-World game_world = new World();
-string player;
-while (true)
+class Program
 {
-    Console.WriteLine("please input your name");
-    player = Console.ReadLine();
-    if (player is not null & player.Length > 0)
+    public static void Main()
     {
-        break;
+        World game_world = new World();
+        string player;
+        while (true)
+        {
+            Console.WriteLine("please input your name");
+            player = Console.ReadLine();
+            if (player is not null & player.Length > 0)
+            {
+                break;
+            }
+        }
+        Console.WriteLine($"you are {player}. \nYou are currently at {game_world.current_location.Location_Name}");
+        Console.WriteLine(game_world.current_location.Location_Description);
+        while (true)
+        {
+            break;
+        }
     }
-}
-Console.WriteLine($"you are {player}. \nYou are currently at {game_world.current_location.Location_Name}");
-Console.WriteLine(game_world.current_location.Location_Description);
+}    
